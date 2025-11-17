269f2674a8443354a93eb276fc6209fe88f7c059
zhanjh <miaque@163.com>
2025-11-17 17:42:44 +0800

    首次提交

---
+++ configs/app_config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import SettingsConfigDict
+
+
+class AppConfig:
+    model_config = SettingsConfigDict(
+        # read from dotenv format config file
+        env_file=".env",
+        env_file_encoding="utf-8",
+        # ignore extra attributes
+        extra="ignore",
+    )
